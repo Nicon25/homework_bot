2d108dec70530f581aa22519e74153f3f65a8b85
Nicon25 <119941789+Nicon25@users.noreply.github.com>
2023-10-12 21:20:42 +0100

    Update exceptions.py

--- exceptions.py
+++ exceptions.py
@@ -1,12 +1,12 @@
 class HTTPRequestError(Exception):
     def __init__(self, response):
-        message = (f'Ошибка {response.status_code}]')
+        message = (f'Error {response.status_code}]')
         super().__init__(message)
 
 
 class ParseStatusError(Exception):
     def __init__(self, homework_status):
         message = (
-            (f'Неизвестный статус работы: {homework_status}')
+            (f'Unknown work status: {homework_status}')
         )
         super().__init__(message)
